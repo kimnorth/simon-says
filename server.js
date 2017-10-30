8e90ee7542f8ba8b10dffc069c8d26263e9110ca
Kim North <mickeymirage@hotmail.com>
2017-10-30 11:24:07 +0000

    Initial commit. Server running and serving static page.

---
+++ server.js
@@ -0,0 +1,27 @@
+
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var express = require('express');
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var app = express();
+var server = http.createServer(app);
+
+app.use('/', express.static('client'))
+
+// router.get('/', function (req, res) {
+//   res.send('client')
+// })
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+});
+
