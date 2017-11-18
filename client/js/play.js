e0f5b9e3ba742500540cd3ff65ffeff6c678c525
Kim North <mickeymirage@hotmail.com>
2017-11-18 22:32:17 +0000

    experimenting with modules, etc, for abstraction.

--- client/js/play.js
+++ client/js/play.js
@@ -1,7 +1,20 @@
 var playState = {
     
+    test: function(){
+        console.log("Test works")
+    },
+    
+    preload: function(){
+        this.testObject = {
+            testFunction: function(){
+                console.log("Test function works.")
+            }
+        };
+    },
+    
     create: function(){
-        
+        this.test();
+        this.testObject.testFunction();
     }
     
 }
---
+++ client/js/test.js
@@ -0,0 +1,7 @@
+class Test {
+    
+    static testMethod(){
+        console.log("This is being printed.")
+    }
+    
+}
