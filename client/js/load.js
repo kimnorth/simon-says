925a6dbc6f25759544da5eedf4dcf8b2b2322244
Kim North <mickeymirage@hotmail.com>
2017-11-20 22:38:28 +0000

    Adding method to randomly pick a colour - currently a string - and pushing to an array of colours representing the sequence within the game.

--- client/js/load.js
+++ client/js/load.js
@@ -1,13 +1,19 @@
 var loadState = {
     
     preload: function(){
+        
         try {
             console.log('Loading assets.');
+            
             game.load.image('background', '../img/wood.jpg');
+            
             game.load.image('red-square', '../img/red_square.png');
             game.load.image('blue-square', '../img/blue_square.png');
             game.load.image('green-square', '../img/green_square.png');
             game.load.image('yellow-square', '../img/yellow_square.png');
+            
+            // console.log('Context of load screen:', this)
+
             console.log('Assets loaded.');
         }
         catch(err){
--- client/js/play.js
+++ client/js/play.js
@@ -1,32 +1,78 @@
+var background;
+var redSquare;
+var yellowSquare;
+var greenSquare;
+var blueSquare;
+
+// var testClass = class {
+
+       
+// }
+
 var playState = {
-    
-    test: function(){
-        console.log("Test function works")
-    },
     
     preload: function(){
         
         this.testObject = {
             testFunction: function(){
                 console.log("Test function works.")
-            }
-        };
+                console.log(this)
+            }.bind(this)
+        },
+        
+        // **** Game Logic Object *** //
+        
+        this.gameLogic = {
+            
+            // Will eventually contain hexcodes for colours. Probably need to
+            // change sprites to graphics drawn by phaser.
+            
+            colours: [
+              "blue",
+              "red",
+              "yellow",
+              "green"
+            ],
+            
+            colourSequence: [],
+            
+            getRandomNumber: function(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+            },
+            
+            randomNum: this.gameLogic,
+            
+            pickRandomColour: function(){
+                var randomNum = this.gameLogic.getRandomNumber(1, 4);
+                this.gameLogic.colourSequence.push(this.gameLogic.colours[randomNum-1])
+                for (let colour of this.gameLogic.colourSequence){
+                    console.log(colour)
+                }
+                console.log("Array of picked colours:", this.gameLogic.colourSequence);
+            }.bind(this)
+        }
         
     },
     
     create: function(){
-        this.test();
-        this.testObject.testFunction();
-        game.add.sprite(0, 0, 'background');
-        game.add.sprite(100, 100, 'red-square');
-        game.add.sprite(600, 100, 'blue-square');
-        game.add.sprite(100, 400, 'green-square');
-        game.add.sprite(600, 400, 'yellow-square');
+        
+        // Testing Functions, Objects
+
+        background = game.add.sprite(0, 0, 'background');
+        redSquare = game.add.sprite(100, 100, 'red-square');
+        blueSquare = game.add.sprite(600, 100, 'blue-square');
+        greenSquare = game.add.sprite(100, 400, 'green-square');
+        yellowSquare = game.add.sprite(600, 400, 'yellow-square');
+        
+        redSquare.inputEnabled = true;
+        redSquare.events.onInputDown.add(this.clicked, this);
+        
+    },
+    
+    clicked: function(){
+        this.gameLogic.pickRandomColour(1, 4);
     }
     
-}
-
-var testClass = class {
-
-       
 }
